974bb95485da43914a0d0ecb71a7e91b0bee98af
Justin Pradier <chromecide@chromecide.com>
2013-11-06 14:25:10 +1100

    added code from Part 4 of the Home Network of Things

---
+++ index.js
@@ -0,0 +1,22 @@
+//assign the args to a variable
+var args = process.argv;
+
+var thingConfig = {};
+
+//loop through the arguments
+//we start at position 2, because the first item is "node" and the second item is the script name
+for(var itemIdx=2;itemIdx<args.length;itemIdx++){
+
+  var argItem = args[itemIdx];
+
+  // split the current item on an equals sign, 
+  // this will result in an array with item 0 being the key
+  // and item 1 being the key value
+
+  var arg = argItem.split('=');
+
+  thingConfig[arg[0]] = arg[1];
+
+}
+
+console.log(thingConfig);
